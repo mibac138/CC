a83ce1dc11a5e2a961f5328d4a7b3fe488004029
Michael <mibac831@wp.pl>
2013-12-26 16:38:17 +0100

    Update For GitHubAPI
    New, updated version for GitHubAPI, changes:
    + Completed Getters And Setters
    + Minor Changes And Fixes

--- APIs/GitHubAPI.lua
+++ APIs/GitHubAPI.lua
@@ -1,20 +1,60 @@
 local GitHubHttp = "http://github.com"
 local GitHubSourceHttp = "http://raw.github.com"
 local GitHubUserName = "mibac138"
+local GitHubRepoName = "CC"
 
-function setGitHubUserName(user)
- user = tostring(user)
- GitHubUserName = user
+-- Getters And Setters
+
+-- GitHubUserName
+
+function setGitHubUserName(_GitHubUserName)
+ local GitHubUserName = tostring(_GitHubUserName)
 end
 
 function getGitHubUserName()
  return GitHubUserName
 end
 
-function setGitHubHttpAddress(GitHubHttp)
- GitHubHttp = tostring(GitHubHttp)
- if not string.sub(GitHubHttp, 1, 7) == "http://" then
-  GitHubHttp = "http://" .. GitHubHttp
+-- GitHubHttpAddress
+
+function setGitHubHttpAddress(_GitHubHttp)
+ local _GitHubHttp = tostring(_GitHubHttp)
+ 
+ if not string.sub(_GitHubHttp, 1, 7) == "http://" then
+  _GitHubHttp = "http://" .. _GitHubHttp
  end
  
+ GitHubHttp = _GitHubHttp
 end
+
+function getGitHubHttpAddress()
+ return GitHubHttp
+end
+
+-- GitHubSourceHttpAddress
+
+function setGitHubSourceHttpAddress(_GitHubSourceHttp)
+ local _GitHubSourceHttp = tostring(_GitHubSourceHttp)
+ 
+ if not string.sub(_GitHubSourceHttp, 1, 7) == "http://" then
+  _GitHubSourceHttp = "http://" .. _GitHubSourceHttp
+ end
+ 
+ GitHubSourceHttp = _GitHubSourceHttp
+end
+
+function getGitHubSourceHttpAddress()
+ return GitHubSourceHttp
+end
+
+-- GitHubRepo[sitory]Name
+
+function setGitHubRepoName(_GitHubRepoName)
+ GitHubRepoName = tostring(_GitHubRepoName)
+end
+
+function getGitHubRepoName()
+ return GitHubRepoName
+end
+
+setGitHubRepositoryName = setGitHubRepoName
