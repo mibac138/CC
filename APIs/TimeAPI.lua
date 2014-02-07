6102a2e0c98b18828f08a32af42b672e6d366a4c
Michael <mibac831@wp.pl>
2014-02-07 15:49:19 +0100

    Initial Release 1.0 Of TimeAPI

--- APIs/TimeAPI.lua
+++ APIs/TimeAPI.lua
@@ -1,43 +1,32 @@
 --[[
- My, mibac138 small Time API, inspired by surferpup's "Real World Time API".
+ My, mibac138 small Time API.
+ 
 ]]
 
 local Version = 1.0
 local format = "2:1 4 3 5"
-local website = "http://www.timeapi.org/"
-local TZ = "utc/" -- Time Zone
-local websiteParams = "now?format={[%22sec%22]=%25S,%20[%22time%22]=%22%25H:%25M%22,%20[%22day%22]=%25d,%20[%22month%22]=%22%25b%22,%20[%22year%22]=%25Y}"
-local timeout = -1
-local http = false
+local website = "http://www.timeapi.org"
+local timezone = "utc"
+local websiteParams = "now?format={%22%25S%22,%20%22%25H:%25M%22,%20%22%25d%22,%20%22%25b%22,%20%22%25Y%22}"
 
-if not http then error("TimeAPI: HTTP API Not Enabled! TimeAPI Disabled!", 0) else http = true end
+if not http then error("TimeAPI: HTTP API Not Enabled! TimeAPI Disabled!", 0) end
 
 --[[ Helper Functions ]]--
 
-local function getWebsite(_website)
+local function getWebsite(httpAddress)
  if not http then return false, "HTTP API Not Enabled!" end
+ ok, data = false, nil
  
- website = _website or website
- ok, data = pcall(function(website) return http.get(website).readAll() end)
+ ok, data = pcall(function() return http.get(httpAddress).readAll() end)
  
  return ok, data
-end
-
-local function initTimer(_timeout)
- timeout = _timeout or timeout
- 
- if timeout > 0 then
-  sleep(timeout)
- end
- 
- return false, "timeout"
 end
 
 --[[ Getters And Setters ]]--
 
 --+ TimeFormat +--
 
-function setTimeFormat(formatTable)
+function setTimeAPITimeFormat(formatTable)
  if formatTable then
   format = ""
   
@@ -53,13 +42,13 @@ function setTimeFormat(formatTable)
  return false
 end
 
-function getTimeFormat()
+function getTimeAPITimeFormat()
  return format
 end
 
 --+ Website +--
 
-function setWebsite(_website)
+function setTimeAPIWebsite(_website)
  if website then
   website = _website
   
@@ -69,13 +58,13 @@ function setWebsite(_website)
  return false
 end
 
-function getWebsite()
+function getTimeAPIWebsite()
  return website
 end
 
 --+ Website params +--
 
-function setWebsiteParams(_websiteParams)
+function setTimeAPIWebsiteParams(_websiteParams)
  if _websiteParams then
   websiteParams = websiteParams
   
@@ -87,7 +76,7 @@ end
 
 --+ Timeout +--
 
-function setTimeout(_timeout)
+function setTimeAPITimeout(_timeout)
  if _timeout and type(_timeout) == "number" then
   timeout = _timeout
  
@@ -97,39 +86,58 @@ function setTimeout(_timeout)
  return false
 end
 
-function getTimeout()
+function getTimeAPITimeout()
  return timeout
+end
+
+--+ Version +--
+
+function getTimeAPIVersion()
+ return Version
 end
 
 --[[ Functions ]]--
 
-function string:Time(_data, _website, _timezone, _websiteParams, _timeout)
- data = _data or {get(true, _website, _timezone, _websiteParams, _timeout)}
- 
- if data[1] ~= true then return data[1], data[2] end
- 
- for i=1, #data do
-  self = self:gsub("%"..tostring(i), data[i])
- end
- 
- return self
-end
-
-function get(tableFormatIsInNumbers, _website, _timezone, _websiteParams, _timeout)
- local website, timezone, websiteParams, timeout = _website or website, _timezone or timezone, _websiteParams or websiteParams, _timeout or timeout
- local httpAddress = website .. timezone .. websiteParams
- local ok, data = parallel.waitForAny(getWebsite(website), initTimer(timeout))
+function getTime(str, _website, _timezone, _websiteParams)
+ ok, data = getRawTime(true, _website, _timezone, _websiteParams)
  
  if not ok then return ok, data end
  
- local websiteData = textutils.unserialize(http.get(data))
- local data = {}
+ for i=1, #data do
+  str = string.gsub(str, "%^"..i, tostring(data[i]))
+  --print(str)
+ end
+
+ return str
+end
+
+function string:getTime(_website, _timezone, _websiteParams)
+ ok, data = getRawTime(true, _website, _timezone, _websiteParams)
+ 
+ if not ok then return ok, data end
+ 
+ for i=1, #data do
+  self = string.gsub(self, "%^"..i, tostring(data[i]))
+  --print(str)
+ end
+
+ return self
+end
+
+function getRawTime(tableFormatIsInNumbers, _website, _timezone, _websiteParams)
+ local website, timezone, websiteParams, httpAddress = _website or website, _timezone or timezone, _websiteParams or websiteParams, website .. "/" .. timezone .. "/" .. websiteParams
+ 
+ ok, _data = getWebsite(httpAddress)
+ 
+ if not ok then return ok, _data end
  
  if tableFormatIsInNumbers then
-  data = {websiteData["sec"], websiteData["time"], websiteData["day"], websiteData["month"], websiteData["year"]}
+  return ok, textutils.unserialize(_data)
  else
-  data = {["sec"] = websiteData["sec"], ["time"] = websiteData["time"], ["day"] = websiteData["day"], ["month"] = websiteData["month"], ["year"] = websiteData["year"]}
+  data = {["sec"] = _data[1], ["time"] = _data[2], ["day"] = _data[3], ["month"] = _data[4], ["year"] = _data[5]}
+  
+  return ok, data
  end
- 
+
  return ok, data
 end
